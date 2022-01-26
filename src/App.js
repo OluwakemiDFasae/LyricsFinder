46c832892ead21fa26b5a4d093a82fd2ed65e8b1
OluwakemiDFasae <92366770+OluwakemiDFasae@users.noreply.github.com>
2022-01-26 13:51:41 +0100

    functions implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from "./context";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route exact path="/lyrics/track/:id" element={<Lyrics />} />
+              </Routes>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
